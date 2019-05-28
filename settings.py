620f97dec5a568c2f19e418a77f0beab61824b3c
Billy Glasbey <40150004+BGlasbey@users.noreply.github.com>
2019-05-28 14:49:58 +0100

    Updated settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 current_environment = {
-    'env': 'sandbox'
+    'env': ''               # sandbox || ecm3
 }
 
 sandbox_client_details = {
-    'client_code': 'SDKTST',
-    'api_key': 'hkZujzFR2907XAtYe6qkKRsBo',
+    'client_code': '',
+    'api_key': '',
 }
 
 ecm3_client_details = {
-    'client_code': 'LUISCT',
-    'api_key': 'xAQdXTPtZPG3QqsUJw73wx1o',
+    'client_code': '',
+    'api_key': '',
 }
 
 # Changing these values without prior discussion with EazyCollect may
@@ -20,16 +20,16 @@ direct_debit_processing_days ={
 }
 
 contracts = {
-    'auto_start_date': False,
-    'auto_fix_ad_hoc_termination_type': False,
-    'auto_fix_ad_hoc_at_the_end': False,
-    'auto_fix_payment_day_in_month': False,
-    'auto_fix_payment_month_in_year': False,
+    'auto_start_date': True,
+    'auto_fix_ad_hoc_termination_type': True,
+    'auto_fix_ad_hoc_at_the_end': True,
+    'auto_fix_payment_day_in_month': True,
+    'auto_fix_payment_month_in_year': True,
 }
 
 payments = {
-    'auto_fix_payment_date': False,
-    'is_credit_allowed': False,
+    'auto_fix_payment_date': True,
+    'is_credit_allowed': True,
 }
 
 warnings = {
@@ -38,5 +38,5 @@ warnings = {
 
 other = {
     'bank_holidays_update_days': 30,
-    'force_schedule_updates': True,
+    'force_schedule_updates': False,
 }
